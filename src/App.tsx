970acd1113dc58024199c847a6bf90d43e51c354
rahilmemdani <rmemdanib@gmail.com>
2025-09-09 16:16:42 +0530

    Added gtag

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -13,32 +13,79 @@ import Contact from "./pages/Contact";
 import Policies from "./pages/Policies";
 import NotFound from "./pages/NotFound";
 import ScrollToTop from "../src/ScrollToTop"
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Layout>
-        <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/catalog" element={<Catalog />} />
-            <Route path="/customization" element={<Customization />} />
-            {/* <Route path="/reviews" element={<Reviews />} /> */}
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/policies" element={<Policies />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const useGtmPageView = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "zoshe_tag",          // your GA4 event name
+      page_path: location.pathname,
+      page_title: document.title,
+    });
+  }, [location]);
+};
+
+// const App = () => (
+//   <QueryClientProvider client={queryClient}>
+//     <TooltipProvider>
+//       <Toaster />
+//       <Sonner />
+//       <BrowserRouter>
+//         <Layout>
+//         <ScrollToTop />
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/catalog" element={<Catalog />} />
+//             <Route path="/customization" element={<Customization />} />
+//             {/* <Route path="/reviews" element={<Reviews />} /> */}
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/policies" element={<Policies />} />
+//             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+//             <Route path="*" element={<NotFound />} />
+//           </Routes>
+//         </Layout>
+//       </BrowserRouter>
+//     </TooltipProvider>
+//   </QueryClientProvider>
+// );
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <ScrollTracking /> {/* call the hook here */}
+          <Layout>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/catalog" element={<Catalog />} />
+              <Route path="/customization" element={<Customization />} />
+              {/* <Route path="/reviews" element={<Reviews />} /> */}
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/policies" element={<Policies />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
+
+// Separate component to run the hook inside BrowserRouter
+const ScrollTracking = () => {
+  useGtmPageView();
+  return null;
+};
 
 export default App;
